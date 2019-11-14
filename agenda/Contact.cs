eab427a13ae4bfdda4b268c6d7caed65981af2da
Arceus <49156328+Ange-hells@users.noreply.github.com>
2019-11-14 10:33:13 +0100

    Add files via upload

---
+++ agenda/Contact.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.Serialization;
+using System.Xml.Serialization;
+
+namespace agenda
+{
+	class Contact : INotifyPropertyChanged
+	{
+		private string leNom;
+		private string lePrenom;
+		private string leMail;
+		private string leMobile;
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		//public Contact()
+		//{ }
+
+		public Contact(string unNom, string unPrenom, string unEmail, string unMobile)
+		{
+			leNom = unNom;
+			lePrenom = unPrenom;
+			leMail = unEmail;
+			leMobile = unMobile;
+		}
+
+		public string Nom
+		{
+			get { return leNom; }
+			set { leNom = value;}
+		}
+
+		public string prenom
+		{
+			get { return lePrenom; }
+			set { lePrenom = value;}
+		}
+
+		public string mail
+		{
+			get { return leMail; }
+			set { leMail = value;}
+		}
+
+		public string mobile
+		{
+			get { return leMobile; }
+			set { leMobile = value;}
+		}
+	}
+	//PropertyChanged(this, newPropertyChangedEventArgs(propertyName));
+}
---
+++ agenda/ListeContacts.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.IO.IsolatedStorage;
+using System.Xml.Serialization;
+using System.Runtime.Serialization;
+using System.Collections.ObjectModel;
+
+namespace agenda
+{
+	class ListeContacts
+	{
+		private List<Contact> ListeItems;
+
+		public ListeContacts()
+		{
+			ListeItems = new List<Contact> { };
+		}
+
+		public List<Contact> Items
+		{
+			get { return ListeItems; }
+			set { ListeItems = value;}
+		}
+
+		public void Sauvgarder()
+		{
+			using (IsolatedStorageFile store = IsolatedStorageFile.GetUserStoreForApplication())
+			{
+				if (!store.DirectoryExists("Agenda"))
+				{
+					store.CreateDirectory("Agenda");
+				}
+
+				//creation d'un flux pour ecrire un fichier
+				using (IsolatedStorageFileStream fileStream = store.OpenFile("Agenda/Contact.xml", System.IO.FileMode.OpenOrCreate, System.IO.FileAccess.Write))
+				{
+					//serialisation XML du flux dans le fichier
+					XmlSerializer Serializer = new XmlSerializer(typeof(ListeContacts));
+					Serializer.Serialize(fileStream, this);
+				}
+
+			}
+
+		}
+	}
+}
